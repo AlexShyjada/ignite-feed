0d736c799f16366c99a0b6d703745cba3223d373
Alexandre Shyjada <alexandre.shyjada@ufba.br>
2022-09-06 20:43:40 -0300

    Profile Container

---
+++ project/src/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+display: flex;
+justify-content: center;
+gap: 1rem;
+padding: 1.25rem;
+background: var(--gray-800);
+font-weight: 700;
+font-size: 1.5rem;
+line-height: 160%;
+`
